78326b6df94a2c9d8eeede34b55e7ea0e570bcae
KatheCast <kathiscast1219@gmail.com>
2024-11-08 01:04:16 -0500

    Adición login

---
+++ blog-destinos-turisticos/src/components/iniciarSesion.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const valid_users = [
+  {
+    email: "gewurog@mailinator.com",
+    password: "Pa$$w0rd!",
+    id: 3,
+  },
+  {
+    email: "qatygecuno@mailinator.com",
+    password: "Pa$$w0rd!",
+    id: 5,
+  },
+];
+
+export const IniciarSesion = () => {
+  const urlApi = "https://67263d98302d03037e6cdf7e.mockapi.io/api";
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      navigate("/listaDePosts");
+    }
+  }, []);
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const user = valid_users.find(
+      (user) => user.email === email && user.password === password
+    );
+    const userForApi = await fetch(`${urlApi}/users/${user.id}`);
+    localStorage.setItem("user", JSON.stringify(await userForApi.json()));
+    navigate("/nuevoPost");
+  };
+
+  const goRegister = () => {
+    navigate("/register");
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Iniciar sesión</h2>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          placeholder="Correo electrónico"
+          name="email"
+          onChange={onChange}
+        ></input>
+        <input
+          type="password"
+          placeholder="Contraseña"
+          name="password"
+          onChange={onChange}
+        ></input>
+        <button type="submit">Iniciar sesión</button>
+        <h3 onClick={goRegister}>¿No tienes cuenta? registrate</h3>
+      </form>
+    </div>
+  );
+};
