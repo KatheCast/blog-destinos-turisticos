9726b90ef2d0b63a3589a2c82936550e2b7c6526
KatheCast <kathiscast1219@gmail.com>
2024-11-11 14:10:55 -0500

    Prueba API

--- blog-destinos-turisticos/src/components/listaDePosts.jsx
+++ blog-destinos-turisticos/src/components/listaDePosts.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 export const ListaDePosts = () => {
   const urlApi = "https://6622071827fcd16fa6c8818c.mockapi.io/api/v1";
-  const [posts, setPosts] = useState([]);
+  const [blogs, setBlogs] = useState([]);
   const [users, setUsers] = useState([]);
   const navigate = useNavigate();
 
@@ -13,14 +13,14 @@ export const ListaDePosts = () => {
     setUsers(data);
   };
 
-  const getPosts = async () => {
-    const response = await fetch(`${urlApi}/posts`);
+  const getBlogs = async () => {
+    const response = await fetch(`${urlApi}/blogs`);
     const data = await response.json();
     data.forEach((post) => {
-      const user = users.find((user) => user.id === post.userId);
+      const user = users.find((user) => user.id === post.creator);
       post.user = user;
     });
-    setPosts(data);
+    setBlogs(data);
   };
 
   useEffect(() => {
@@ -28,7 +28,7 @@ export const ListaDePosts = () => {
   }, []);
 
   useEffect(() => {
-    getPosts();
+    getBlogs();
   }, [users]);
 
   const goToNewPost = () => {
@@ -39,15 +39,15 @@ export const ListaDePosts = () => {
     <div className="timeline-container">
       <h2>Timeline</h2>
       <button onClick={() => goToNewPost()}>Nuevo post</button>
-      {posts.map((post) => (
+      {blogs.map((post) => (
         <div key={post.id} className="post">
           {post.user && (
             <a href={`/users/${post.user.id}`}>
               <img src={`${post.user?.avatar}/${post.user?.id}`} alt="avatar" />
             </a>
           )}
-          <h3>{post.title}</h3>
-          <p>{post.content}</p>
+          <h3>{post.name}</h3>
+          <p>{post.review}</p>
           <small>publicado por: {post.user?.name}</small>
         </div>
       ))}
--- blog-destinos-turisticos/src/components/nuevoPost.jsx
+++ blog-destinos-turisticos/src/components/nuevoPost.jsx
@@ -4,60 +4,60 @@ import { EditIcon } from "./editIcon";
 
 export const NuevoPost = () => {
   const urlApi = "https://6622071827fcd16fa6c8818c.mockapi.io/api/v1";
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [posts, setPosts] = useState([]);
+  const [name, setName] = useState("");
+  const [review, setReview] = useState("");
+  const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState({});
   const [toEditPost, setToEditPost] = useState(null);
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
     setUser(user);
-    fetch(`${urlApi}/posts`)
+    fetch(`${urlApi}/blogs`)
       .then((response) => response.json())
-      .then((data) => setPosts(data.filter((post) => post.userId === user.id)));
+      .then((data) => setBlogs(data.filter((post) => post.creator === user.id)));
   }, []);
 
   const onChange = (e) => {
     const { name, value } = e.target;
-    if (name === "title") {
-      setTitle(value);
-    } else if (name === "content") {
-      setContent(value);
+    if (name === "name") {
+      setName(value);
+    } else if (name === "review") {
+      setReview(value);
     }
   };
 
   const onSubmit = async (e) => {
     e.preventDefault();
     const data = {
-      title,
-      content,
-      userId: user.id,
+      name,
+      review,
+      creator: user.id,
     };
-    const postCreated = await fetch(`${urlApi}/posts`, {
+    const postCreated = await fetch(`${urlApi}/blogs`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
-    setPosts([...posts, await postCreated.json()]);
+    setBlogs([...blogs, await postCreated.json()]);
   };
 
   const deletePost = (id) => {
-    fetch(`${urlApi}/posts/${id}`, {
+    fetch(`${urlApi}/blogs/${id}`, {
       method: "DELETE",
     });
-    setPosts(posts.filter((post) => post.id !== id));
+    setBlogs(blogs.filter((post) => post.id !== id));
   };
 
   const editPost = (id) => {
-    const post = posts.find((post) => post.id === id);
+    const blogs = blogs.find((post) => post.id === id);
     setToEditPost(post);
   };
 
   const sendToEditPost = async () => {
-    const postEdited = await fetch(`${urlApi}/posts/${toEditPost.id}`, {
+    const postEdited = await fetch(`${urlApi}/blogs/${toEditPost.id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -65,7 +65,7 @@ export const NuevoPost = () => {
       body: JSON.stringify(toEditPost),
     });
     const postEditedJson = await postEdited.json();
-    setPosts(posts.map((post) => (post.id === postEditedJson.id ? postEditedJson : post)));
+    setBlogs(blogs.map((post) => (post.id === postEditedJson.id ? postEditedJson : post)));
     setToEditPost(null);
   };
 
@@ -73,28 +73,28 @@ export const NuevoPost = () => {
     <div className="new-post-container">
       <h2>Nuevo Post</h2>
       <h3>{user.name}</h3>
-      <h3>Tienes {posts.length} posts publicados</h3>
+      <h3>Tienes {blogs.length} posts publicados</h3>
       <form onSubmit={onSubmit}>
         <input
           type="text"
-          placeholder="Titulo"
-          name="title"
+          placeholder="Nombre"
+          name="name"
           onChange={onChange}
         ></input>
         <textarea
-          placeholder="Contenido"
-          name="content"
+          placeholder="Review"
+          name="review"
           onChange={onChange}
         ></textarea>
         <button type="submit">Publicar</button>
       </form>
 
-      <div className="posts-container">
-        {posts.map((post) =>
+      <div className="blogs-container">
+        {blogs.map((post) =>
           toEditPost && toEditPost.id === post.id ? (
             <div className="post" key={post.id}>
-              <input type="text" value={toEditPost.title} onChange={(e) => setToEditPost({ ...toEditPost, title: e.target.value })} />
-              <input type="text" value={toEditPost.content} onChange={(e) => setToEditPost({ ...toEditPost, content: e.target.value })} />
+              <input type="text" value={toEditPost.name} onChange={(e) => setToEditPost({ ...toEditPost, name: e.target.value })} />
+              <input type="text" value={toEditPost.review} onChange={(e) => setToEditPost({ ...toEditPost, review: e.target.value })} />
               <button onClick={() => sendToEditPost()}>Guardar</button>
             </div>
           ) : (
@@ -107,8 +107,8 @@ export const NuevoPost = () => {
                   <EditIcon />
                 </div>
               </div>
-              <h4>{post.title}</h4>
-              <p>{post.content}</p>
+              <h4>{post.name}</h4>
+              <p>{post.review}</p>
             </div>
           )
         )}
